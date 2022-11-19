fbbe544af5dc5f38920ac0b4c49235c23b672e6a
djanders19 <davidias.j.a@gmail.com>
2022-11-19 15:16:58 -0500

    Update cost matrix to default to zero for lack of cnxn

--- load_dataset.py
+++ load_dataset.py
@@ -7,20 +7,29 @@ class gMission:
     Return the workers,tasks and cost matrix of workers and tasks
     """
     def load(self):
+        """
+        :return (workers, tasks, cost) - workers is a set of all the workers, while tasks is a set
+        of all tasks. The Costs is a worker by task cost array where the [worker][task]th item is
+        the expected value of matching the worker to a task.
+        """
         f_edges = open(gMission_edges, "r")
         edgeWeights = dict()
         for line in f_edges:
             vals = line.split(",")
-            edgeWeights[vals[0]] = vals[1].split("\n")[0]
+            edgeWeights[vals[0]] = vals[1].strip()
         w = np.array(list(edgeWeights.values()), dtype="float")
         max_w = max(w)
         edges = {k: (float(v) / float(max_w)) for k, v in edgeWeights.items()}
         workers = set([int(float(key.split(";")[0])) for key in edges])
         tasks = set([int(float(key.split(";")[1])) for key in edges])
-        cost = np.ones((max(workers)+1, max(tasks)+1))
+        cost = np.zeros((max(workers)+1, max(tasks)+1))
         for k,v in edges.items():
             i,j = k.split(";")
             i = int(float(i))
             j = int(float(j))
             cost[i][j] = v
         return workers,tasks,cost
+
+if __name__ == '__main__':
+    a = gMission()
+    a.load()
