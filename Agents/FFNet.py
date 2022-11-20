3ffef82b48cc4b7ff215ceeb088f12bec2714094
djanders19 <davidias.j.a@gmail.com>
2022-11-20 16:03:04 -0500

    Documentation for OLBMReinforceTrainer

--- Agents/FFNet.py
+++ Agents/FFNet.py
@@ -75,50 +75,77 @@ class OLBMReinforceTrainer:
                                                                num_workers=self.num_workers,
                                                                random_seed=problem_generator_seed)
 
-        log_probs = []  # vector of log-probabilities
+        log_probs = []  # vector of log-probabilities of model outputs for each step of the problem
         rewards = []  # vector of rewards
 
-        # Use policy (neural network) to complete OLBM problem, recording action probabilities from policy, reward
-        # from environment, and action at each step
+        # Use policy (neural network) to complete OLBM problem, recording action probabilities from policy, the action
+        # that was taken, and the reward for taking that action (in this context, reward is the weight of the edge that
+        # we choose to add to the network):
         while problem.has_unseen_workers():
-            worker, state = problem.get_next_nn_input()
-            state = torch.from_numpy(state).to(DEVICE)
+            worker, state = problem.get_next_nn_input()  # Pick the next worker to match and get the input as a vector
+            state = torch.from_numpy(state).to(DEVICE)  # Need to convert the datatype to a tensor for pytorch
             action, log_prob = self.model(state)  # Choose an action based on the model
-            reward = problem.match(action, worker)
+            reward = problem.match(action, worker)  # Perform matching, calculate reward
 
-            rewards.append(reward)
-            log_probs.append(log_prob)
+            rewards.append(reward)  # Keep track of the reward we got for taking the action with highest log-prob
+            log_probs.append(log_prob)  # Keep track of associated model output that generated the above reward
 
+        # Keep track of "total reward" generated throughout the iteration for plotting later. We'll want to see these
+        # numbers going up over time.
         self.all_rewards.append(np.sum(rewards))
 
-        # Calculate discounted rewards for each action
+        # Calculate "discounted rewards" for each action. At a high level, the reason for doing this is because each
+        # action should take into account not just the reward generated for immediately taking the action, but also
+        # all the future rewards that are gained *as a result* of taking this action - in other words, the rewards of
+        # *future* actions. However, it's assumed (in the Bellman Eqn.) that *future* rewards matter *less* than
+        # immediate rewards, so we caclulate the "total discounted reward" at each time step as the sum of the rewards
+        # generated in the future, discounted at a rate of gamma**t * reward_t, where t is the number of time steps
+        # into the future.
         discounted_rewards = []
         for t in range(len(rewards)):
-            Gt = 0
+            discounted_reward_at_time_t = 0
             exponent = 0
 
+            # calculate reward for t'th action as reward for t'th action plus sum of future discounted actions:
             for reward in rewards[t:]:
-                Gt = Gt + self.gamma ** exponent * reward  # REINFORCE/Bellman Eqn
+                # Bellman Eqn:
+                discounted_reward_at_time_t = discounted_reward_at_time_t + self.gamma ** exponent * reward
                 exponent += 1
-            discounted_rewards.append(Gt)
+            discounted_rewards.append(discounted_reward_at_time_t)
         discounted_rewards = np.array(discounted_rewards)  # Cast to np.array
 
         # Adjust weights of Policy (NN) by backpropagating error to increase rewards:
         _discounted_rewards = torch.tensor(discounted_rewards, dtype=torch.float32, device=DEVICE)
+
+        # Center _discounted_rewards to keep gradients from exploding:
         discounted_rewards = (_discounted_rewards - torch.mean(_discounted_rewards)) / (torch.std(_discounted_rewards))
         log_prob = torch.stack(log_probs)
 
+        # This conditional is necessary to check for the case where we've made a problem instance that has zero matching
+        # workers and tasks. In this case, reward will be
         if torch.isnan(discounted_rewards).all():
+            # This conditional is necessary to check for the case where we've made a problem instance that has zero
+            # matching workers and tasks. In this case, _discounted_rewards will be entirely 0s, so the centering
+            # above will cause a divide-by-zero error and a bunch of NaNs. In reality, we want the gradient to be 0
+            # since we don't really learn anything from these cases
             policy_gradient = -log_prob.to(DEVICE) * torch.zeros_like(discounted_rewards).to(DEVICE)
         else:
+            # In the REINFORCE algorithm, we are trying to maximize the Expected Reward. This can be calculated as the
+            # product of the probability of each action at each time step (stored in log_prob) * the discounted-reward
+            # for each action taken at each time-step, which we calculated as discounted_rewards above.
             policy_gradient = -log_prob.to(DEVICE) * discounted_rewards
 
+        # Perform backpropagation:
         self.model.zero_grad()
         policy_gradient.sum().backward()
         self.optimizer.step()
         return np.sum(rewards)
 
     def train_N_iterations(self, N=100):
+        """
+        Perform N training iterations. A single iteration consists of training the network on a single OLBMInstance
+        sampled from GMission.
+        """
         rolling_avg = deque(maxlen=100)
         for episode in range(N):
             reward = self.train_iteration(problem_generator_seed=episode)
